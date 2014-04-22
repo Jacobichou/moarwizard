b0b8a4f77a5d1fe53512237810d38ba08b7401fd
Jacob <jacob.pagano@gmail.com>
2014-04-21 19:08:52 -0500

    setting up static welcome pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+   #Return base title on individual-page basis
+   def full_title(page_title)
+      base_title = "EMIS"
+      if page_title.empty?
+         base_title
+      else
+         "#{base_title} | #{page_title}"
+      end
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Moarwizard::Application.routes.draw do
-  get "static_pages/welcome"
-  get "static_pages/about"
+  root 'static_pages#welcome'
+  
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
