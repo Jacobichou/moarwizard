7b1da7196346f37f833bb2d796e4a010e1f94244
Jacob <jacob.pagano@gmail.com>
2014-04-23 11:21:54 -0500

    update profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+   before_filter :configure_permitted_parameters, if: :devise_controller?
+
+   # def update_sanitized_params
+   #    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:dob, :phone, :address, 
+   #                                                 :insurance)}
+   # end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -6,4 +12,12 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
    dashboard_path
   end
+
+  protected
+
+   def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit({ roles: [] },
+                                                       :email, :password, :password_confirmation, :current_password,
+                                                       :dob, :phone, :address, :insurance) }
+   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140423151617) do
+ActiveRecord::Schema.define(version: 20140423154413) do
 
   create_table "charts", force: true do |t|
     t.integer  "user_id"
@@ -61,6 +61,10 @@ ActiveRecord::Schema.define(version: 20140423151617) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "dob"
+    t.string   "address"
+    t.string   "phone"
+    t.string   "insurance"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
