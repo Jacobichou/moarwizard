82698a203c19a7aa8f40c9d1b3e204fd4f1e928e
Jacob <jacob.pagano@gmail.com>
2014-04-29 11:32:40 -0500

    failed attempt at the appointment conflict avoidance

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,12 +2,20 @@ class AppointmentsController < ApplicationController
   def create
    @appointment = current_user.appointments.create(appointment_params)
    session[:return_to] ||= request.referer
-   if @appointment.save
-      flash[:success] = "Appointment scheduled!"
-      redirect_to session.delete(:return_to)
+
+   if Appointment.exists?(apt_date: @appointment.apt_date)
+    # @appointment.destroy
+    flash[:success] = "This date is already taken!"
+    # taken_datetime(@appointment)
+    redirect_to calendars_path
    else
-      flash[:warning] = "Appointment not scheduled!"
-      render "new"
+     if @appointment.save
+        flash[:success] = "Appointment scheduled!"
+        redirect_to session.delete(:return_to)
+     else
+        flash[:warning] = "Appointment not scheduled!"
+        render "new"
+     end
    end
   end
 
@@ -18,11 +26,12 @@ class AppointmentsController < ApplicationController
   end
 
   def destroy
+
   end
 
   private
 
-  def appointment_params
-    params.require(:chart).permit(:user_id, :apt_date, :apt_time, :attending_physician, :rescheduled)
-  end
+    def appointment_params
+      params.require(:chart).permit(:user_id, :apt_date, :apt_time, :attending_physician, :rescheduled)
+    end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,9 +1,18 @@
 class Appointment < ActiveRecord::Base
+   # after_save ApptCallbacks.new
    # Appointment stores who the appointment is for using user_id.
    # rescheduled is an integer: {0 = no, 1 = yes, 6 = canceled}
    # attending_physician indicates the id of the physician/pa to be seen
    # time and date are stored as datetime objects
 
    has_many :users
-   
+
+   class ApptCallbacks
+     def after_save(record)
+       record.destroy
+     end
+
+     alias_method :after_initialize, :after_save
+   end
+
 end
